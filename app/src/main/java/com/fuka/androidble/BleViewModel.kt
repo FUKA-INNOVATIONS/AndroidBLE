d61e3fa6ce55a9171c188487254425d7dbd44669
Fuwad Kalhori <fuad.kalhori@gmail.com>
2023-01-25 17:14:16 +0200

    ff

---
+++ app/src/main/java/com/fuka/androidble/BleViewModel.kt
@@ -0,0 +1,8 @@
+package com.fuka.androidble
+
+import androidx.lifecycle.ViewModel
+
+
+class BleViewModel() : ViewModel() {
+
+}
--- app/src/main/java/com/fuka/androidble/MainActivity.kt
+++ app/src/main/java/com/fuka/androidble/MainActivity.kt
@@ -1,20 +1,51 @@
 package com.fuka.androidble
 
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.bluetooth.le.BluetoothLeScanner
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanFilter
+import android.bluetooth.le.ScanResult
+import android.bluetooth.le.ScanSettings
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
 import com.fuka.androidble.ui.theme.AndroidBLETheme
+import kotlin.math.log
 
 class MainActivity : ComponentActivity() {
+
+    companion object {
+        private val TAG = "Android FBLE"
+        val BLUETOOTH_REQUEST_CODE = 1
+    }
+
+    private val bluetoothAdapter: BluetoothAdapter by lazy {
+        (getSystemService(BLUETOOTH_SERVICE) as BluetoothManager).adapter
+    }
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         setContent {
             AndroidBLETheme {
                 // A surface container using the 'background' color from the theme
@@ -23,9 +54,33 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
 
+                    Row {
+                        Button(onClick = { Log.d(TAG, "Hello BLE") ;  }) {
+                            Text(text = "Scan BLE devices")
+                        }
+                    }
+
                 }
             }
         }
     }
+
+
+
+
+    private fun hasPermissions(): Boolean {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
+            return false
+        }
+        if (bluetoothAdapter == null || !bluetoothAdapter!!.isEnabled) {
+            Log.d("DBG", "No Bluetooth LE capability")
+            return false
+        } else if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.d("DBG", "No fine location access")
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1); return true // assuming that the user grants permission
+        }
+        return true }
+
+
 }
 
